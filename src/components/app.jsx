64d25f6048cdb97379a5457c7856bcdd9dd8cdfa
delphkim <hgkim9201@gmail.com>
2021-10-26 14:11:48 +0900

    Lifting state up to App Component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,8 +3,65 @@ import Habits from './habits';
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    habits: [
+      {
+        id: Math.random(),
+        name: 'reading',
+        count: 0,
+      },
+      {
+        id: Math.random(),
+        name: 'coding',
+        count: 0,
+      },
+      {
+        id: Math.random(),
+        name: 'running',
+        count: 0,
+      },
+    ],
+  };
+
+  handleIncrease = (habit) => {
+    const newHabits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        return { ...habit, count: habit.count + 1 };
+      } else {
+        return item;
+      }
+    });
+    this.setState((state) => {
+      return { habits: newHabits };
+    });
+  };
+  handleDecrease = (habit) => {
+    const newHabits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        const count = habit.count - 1;
+        return { ...habit, count: count < 0 ? 0 : count };
+      } else {
+        return item;
+      }
+    });
+    this.setState((state) => {
+      return { habits: newHabits };
+    });
+  };
+  handleDelete = (habit) => {
+    const newHabits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState((state) => ({ habits: newHabits }));
+  };
+
   render() {
-    return <Habits />;
+    return (
+      <Habits
+        habits={this.state.habits}
+        onIncrease={this.handleIncrease}
+        onDecrease={this.handleDecrease}
+        onDelete={this.handleDelete}
+      />
+    );
   }
 }
 
